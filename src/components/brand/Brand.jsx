40cc755e6463a1ee6b5f3a2e2744ead2d49785e2
Justice-Udehegbunam <justicefavour491@gmail.com>
2023-11-26 14:56:03 +0100

    Finished with the brands component and styles

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <img src={google} alt="google" />
+      <img src={slack} alt="slack" />
+      <img src={atlassian} alt="atlassian" />
+      <img src={dropbox} alt="dropbox" />
+      <img src={shopify} alt="shopify" />
+    </div>
+  );
 };
 
 export default Brand;
